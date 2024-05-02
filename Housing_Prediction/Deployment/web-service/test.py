ea20da36c61f60442b5d351e010aa076193a479a
Sapphirevic <nyamadie1@gmail.com>
2024-05-01 22:23:25 -0700

    new

---
+++ Housing_Prediction/Deployment/web-service/test.py
@@ -0,0 +1,19 @@
+import requests
+
+price = {"BedroomAbvGr": 4, "GarageCars": 2, "SalePrice": 250000}
+
+# features = predict.prepare_features(price)
+# pred = predict.predict(features)
+# print(pred)
+
+url = 'http://localhost/predict'
+response = requests.post(url, json=price)
+print(response.json())
+# with open("models/lasso.bin", "rb") as f_in:
+#     (dv, model) = pickle.load(f_in)
+
+
+# def predict(features):
+#     X = dv.transform(features)
+#     preds = model.predict(X)
+#     return preds
